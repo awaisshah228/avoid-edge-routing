a0a4bc0a94b58616de49735095256cae5494cf87
awaisshah228 <m.awaisshah228@gmail.com>
2026-03-24 12:13:06 +0500

    fixed issue

--- apps/example/src/main.tsx
+++ apps/example/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ReactFlowProvider } from "@xyflow/react";
+import { inject } from "@vercel/analytics";
 import App from "./App";
+
+inject();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
